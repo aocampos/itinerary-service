fb5d5fdd5ac2ff4da932b61fa22b5fb5cf003265
Alex <aocampos86@gmail.com>
2018-11-25 00:06:10 -0200

    Version the project itry-service-gateway

---
+++ itry-service-gateway/src/main/java/aocampos/itinerary/itryservicegateway/ItryServiceGatewayApplication.java
@@ -0,0 +1,14 @@
+package aocampos.itinerary.itryservicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ItryServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItryServiceGatewayApplication.class, args);
+	}
+}
